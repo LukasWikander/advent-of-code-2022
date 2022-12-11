3b28308896dc740e0ca0c733f5820cdf12254f2f
Lukas Wikander <lukas.wikander@gmail.com>
2022-12-11 11:13:21 +0100

    Added clarifying printout to PuzzleInput class

--- PuzzleInput.hpp
+++ PuzzleInput.hpp
@@ -10,6 +10,7 @@ class PuzzleInput : public std::vector<std::string> {
 public:
     PuzzleInput(const std::string& input) {
         if (std::filesystem::is_regular_file(input)) {
+            std::cout << "Using input file " << input << std::endl;
             std::ifstream is{input};
             std::string line;
             while (std::getline(is, line)) {
@@ -17,6 +18,7 @@ public:
             }
         }
         else {
+            std::cout << "Using data string " << input << std::endl;
             std::istringstream is{input};
             std::string line;
             while (std::getline(is, line)) {
